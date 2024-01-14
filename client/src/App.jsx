74603631dfb0b8d383639ccfb45ff7ffcad08594
Maks <m.posiel@gmail.com>
2024-01-14 12:04:31 +0100

    Added Rooms, startewd routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,89 @@
-import './App.css'
-import io from 'socket.io-client'
-import { useEffect, useState } from 'react'
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Link,
+  Outlet,
+  Route,
+} from "react-router-dom";
+import { Chat } from "./components/Chat";
+import { Home } from "./components/Home";
 
-const socket = io('http://localhost:3001')
 
+
+const socket = io("http://localhost:3001");
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+      <Route element={<Chat />} />
+
+    </Route>)
+  );
+
+  const [room, setRoom] = useState("");
+  const [messageSent, setMessageSent] = useState("");
+  const [messageRecieved, setMessageRecieved] = useState("");
 
   const sendMessage = () => {
-    socket.emit('send_message', "hello");
+    socket.emit("send_message", { messageSent, room });
+  };
+
+  const joinRoom = () => {
+    socket.emit("join_room", { room });
+    console.log("Joined room: " + room);
   };
 
   useEffect(() => {
-    socket.on('recieve_message', (message) => {
-      alert(message);
-    })
+    socket.on("receive_message", (data) => {
+      console.log(data);
+      setMessageRecieved(data.messageSent);
+    });
   }, [socket]);
 
   return (
     <div className="App">
-      <input type="text" placeholder="Message..." />
+      <input
+        type="text"
+        placeholder="Room number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <br />
+      <button onClick={joinRoom}>Join Room</button>
+
+      <br />
+      <input
+        type="text"
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessageSent(event.target.value);
+        }}
+      />
+      <br />
       <button onClick={sendMessage}>Send</button>
+      <br />
+      <div id="message-container">{messageRecieved}</div>
     </div>
-  )
+  );
 }
 
-export default App
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link> <Link to="/data">Chat</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,35 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log(`a user connected: ${socket.id}`);
+io.on("connection", (socket) => {
+  console.log(`a user connected: ${socket.id}`);
 
-    socket.on('send_message', (msg) => {
-        console.log('message: ' + msg);
-        socket.broadcast.emit('recieve_message', msg);
-    });
+  socket.on("send_message", (data) => {
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+    console.log(data);
+
+
+    socket.to(data.room).emit("receive_message", data);
+
+  });
+
+  socket.on("join_room", (data) => {
+    socket.join(data.room);
+    console.log(`user with ID: ${socket.id} joined room: ${data.room}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`user ${socket.id} disconnected`);
+  });
 });
-
 
 server.listen(3001, () => {
-    console.log('listening on port:3001');
-})
-
+  console.log("listening on port:3001");
+});
